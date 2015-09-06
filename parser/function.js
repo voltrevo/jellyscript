236ca36c9e31f203b53a37da0d10df5fdf808108
Andrew Morris <voltrevo@gmail.com>
2015-09-06 18:50:58 +1000

    Avoid ciruclar issues by wrapping function parser

--- parser/function.js
+++ parser/function.js
@@ -1,21 +1,25 @@
 'use strict';
 
-var parser = require('parser');
+module.exports = function() {
+  var parser = require('parser');
 
-var argumentList = require('./argumentList.js');
-var arrow = require('./arrow.js');
-var codeBlock = require('./codeBlock.js');
-var expression = require('./expression');
-var funcKeyword = require('./funcKeyword.js');
+  var argumentList = require('./argumentList.js');
+  var arrow = require('./arrow.js');
+  var codeBlock = require('./codeBlock.js');
+  var expression = require('./expression');
+  var funcKeyword = require('./funcKeyword.js');
 
-module.exports = parser.sequence(
-  parser.wrapOptionalWhitespace(funcKeyword),
-  parser.optional(argumentList),
-  parser.labelledOr(
-    ['expressionBody', parser.sequence(
-      arrow,
-      expression
-    )],
-    ['blockBody', codeBlock]
-  )
-);
+  var function_ = parser.sequence(
+    parser.wrapOptionalWhitespace(funcKeyword),
+    parser.optional(argumentList),
+    parser.labelledOr(
+      ['expressionBody', parser.sequence(
+        arrow,
+        expression
+      )],
+      ['blockBody', codeBlock]
+    )
+  );
+
+  return function_.apply(undefined, arguments);
+};
