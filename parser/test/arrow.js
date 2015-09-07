b90a11f67aa60643743774da77976d0f1cd71d56
Andrew Morris <voltrevo@gmail.com>
2015-09-07 10:57:34 +1000

    Got first tests working (arrow parser)

--- parser/test/arrow.js
+++ parser/test/arrow.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var arrow = require('./arrow.js');
+var arrow = require('../arrow.js');
 var generator = require('./generator.js');
 
 generator('arrow', arrow, {
   valid: [
-    '=>'
+    ['=>']
   ],
   invalid: [
-    '',
-    ' =>',
-    '=> ',
-    '==>'
+    [''],
+    [' =>'],
+    ['=> '],
+    ['==>']
   ]
 });
