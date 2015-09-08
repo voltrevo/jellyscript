3122845db397f42b3fbf6bf626dfd25d8e9b5894
Andrew Morris <voltrevo@gmail.com>
2015-09-08 17:30:59 +1000

    Better webDemo

--- parser/webDemo.js
+++ parser/webDemo.js
@@ -5,19 +5,23 @@ var Stream = require('parser').stream;
 var programParser = require('./program.js');
 
 window.addEventListener('load', function() {
-  var ta = document.createElement('textarea');
+  document.body.style.margin = '0'
+
+  var ta = document.createElement('textarea')
+  ta.style.width = '100vw'
+  ta.style.height = '100vh'
+  ta.style.padding = '10px'
+  ta.style.fontFamily = 'Courier New'
+  ta.style.fontSize = '1em'
+  ta.style.border = '0'
+  ta.style.resize = 'none'
+  ta.style.outline = 'none'
+
   var button = document.createElement('button');
   var result = document.createElement('div');
 
-  ta.style.width = '100vw';
-  ta.style.height = '80vh';
-  ta.style.fontFamily = 'courier new';
-  ta.style.fontSize = '18px';
-  button.style.width = '100vw';
-
   document.body.appendChild(ta);
-  document.body.appendChild(button);
-  document.body.appendChild(result);
+  ta.focus()
 
   ta.value = [
     'console = import("console");',
@@ -27,21 +31,18 @@ window.addEventListener('load', function() {
     'return nil;'
   ].join('\n');
 
-  button.innerHTML = 'Check (cmd + enter)';
-
   var check = function() {
     var start = Date.now();
     var stream = new Stream('{' + ta.value + '}');
     var parseResult = programParser(stream);
     var end = Date.now();
-    result.innerHTML = (parseResult.success ? 'valid!' : 'not valid!') + ' (' + (end - start) + 'ms)';
+
+    var color = (parseResult.success ? '#dfd' : '#fdd');
+    document.body.style.backgroundColor = color;
+    ta.style.backgroundColor = color;
+    console.log('Parse finished in ' + (end - start) + ' ms');
   };
-
-  button.addEventListener('click', check);
-
-  document.addEventListener('keydown', function(evt) {
-    if (evt.keyCode === 13 && evt.metaKey) {
-      check();
-    }
-  });
+  
+  ta.addEventListener('input', check);
+  check();
 });
