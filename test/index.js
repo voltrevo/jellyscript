dd8374e7503baa2e79deea271d65ba1ef2f7255b
Andrew Morris <voltrevo@gmail.com>
2015-09-19 14:00:41 +1000

    Typo

--- test/index.js
+++ test/index.js
@@ -1,3 +1,3 @@
 'use strict';
 
-// TODO (There's really only work on the parser so far, and those tests are in lib/parser/test.
+// TODO (There's really only work on the parser so far, and those tests are in lib/parser/test.)
